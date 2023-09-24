72a3cdc19d1af9c9234eaecb4a60d3ee36057005
SHYAM  SHARMA <shyamsharma@shyams-air.mynetworksettings.com>
2023-09-24 13:55:02 -0400

    Added docker file

--- react-app/vite.config.ts
+++ react-app/vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+  },
+  define: {
+    'process.env': process.env
+  }
 })
